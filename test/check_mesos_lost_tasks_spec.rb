02643e0b6bd0b6b0b0d56ded17196953db856b7f
Avelino <avelino.collar@veinteractive.com>
2017-07-18 10:57:01 +0200

    Fix testing method scope and removed no necessary methods

--- test/check_mesos_lost_tasks_spec.rb
+++ test/check_mesos_lost_tasks_spec.rb
@@ -9,22 +9,15 @@ class MesosLostTasksCheck
   at_exit do
     @@autorun = false
   end
-
-  def critical(*); end
-
-  def warning(*); end
-
-  def ok(*); end
-
-  def unknown(*); end
-end
-
-def check_results(parameters)
-  check = MesosLostTasksCheck.new parameters.split(' ')
-  check.check_tasks(mesos_metrics_response)
 end
 
 describe 'MesosLostTasksCheck' do
+  def check_lost(parameters)
+
+    check = MesosLostTasksCheck.new parameters.split(' ')
+    check.check_tasks(mesos_metrics_response)
+  end
+
   before do
     @default_parameters = '--server localhost --value 0'
     @check = MesosLostTasksCheck.new @default_parameters.split(' ')
@@ -32,13 +25,13 @@ describe 'MesosLostTasksCheck' do
 
   describe '#run' do
     it 'tests that a lost tasks metrics are ok' do
-      tasks_lost = check_results '--server localhost --value 42'
+      tasks_lost = check_lost '--server localhost --value 42'
       expect(tasks_lost).to be 42
     end
 
     it 'tests that an empty server response raises an error' do
-      expect { @check.check_tasks '{}' }.to raise_error(/No metrics for/)
-      expect { @check.check_tasks '' }.to raise_error(/Could not parse JSON/)
+      expect {@check.check_tasks '{}'}.to raise_error(/No metrics for/)
+      expect {@check.check_tasks ''}.to raise_error(/Could not parse JSON/)
     end
   end
 end
--- test/check_mesos_running_task_spec.rb
+++ test/check_mesos_running_task_spec.rb
@@ -7,22 +7,15 @@ class MesosRunningTaskCheck
   at_exit do
     @@autorun = false
   end
-
-  def critical(*); end
-
-  def warning(*); end
-
-  def ok(*); end
-
-  def unknown(*); end
-end
-
-def check_results(parameters)
-  check = MesosRunningTaskCheck.new parameters.split(' ')
-  check.get_running_tasks mesos_metrics_response
 end
 
 describe 'MesosRunningTaskCheck' do
+
+  def check_running(parameters)
+    check = MesosRunningTaskCheck.new parameters.split(' ')
+    check.get_running_tasks mesos_metrics_response
+  end
+
   before do
     @default_parameters = '--server localhost --mode eq --value 0'
     @check = MesosRunningTaskCheck.new @default_parameters.split(' ')
@@ -30,13 +23,13 @@ describe 'MesosRunningTaskCheck' do
 
   describe '#run' do
     it 'tests that running tasks  metrics are ok' do
-      tasks_running = check_results '--server localhost --mode eq --value 0'
+      tasks_running = check_running '--server localhost --mode eq --value 0'
       expect(tasks_running).to be 9
     end
 
     it 'tests that an empty server response raises an error' do
-      expect { @check.get_running_tasks '{}' }.to raise_error(/No tasks in server response/)
-      expect { @check.get_running_tasks '' }.to raise_error(/Could not parse JSON/)
+      expect {@check.get_running_tasks '{}'}.to raise_error(/No tasks in server response/)
+      expect {@check.get_running_tasks ''}.to raise_error(/Could not parse JSON/)
     end
   end
 end
